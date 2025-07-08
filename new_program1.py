1368ffd0bda9844cfc11646f3d6a58bf2f761191
Manjushree-11 <manjushreejadhav33@gmail.com>
2025-07-08 20:24:05 +0530

    Changed by Manjushree

--- new_program1.py
+++ new_program1.py
@@ -1,1 +1,5 @@
 print("Branch")
+driver.find_element(By.XPATH,"//a[contains(text(),'Alert with Textbox') ]").click()
+driver.find_element(By.XPATH,"//button[text()='click the button to demonstrate the prompt box ']").click()
+alert=driver.switch_to.alert
+time.sleep(2)
